6c2584b74a4c6bea9b5801ef577049ea133097c5
sinh <sinhnx@10.22.5.184>
2014-06-09 16:19:17 +0700

    [s01] add jdbc

---
+++ AAD2_Example/src/s01/jdbc/type1/JDBC_ODBC_Bridge.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s01.jdbc.type1;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author SinhNX
+ */
+public class JDBC_ODBC_Bridge {
+    public static void main(String[] args) {
+        try {
+            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+            try (Connection cn = DriverManager.getConnection("jdbc:odbc:type1", "sa", "fat123456@")) {
+                System.out.println("Connect to ODBC complete");
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(JDBC_ODBC_Bridge.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
